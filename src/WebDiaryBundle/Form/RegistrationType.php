9c9aa19b922a374a675e88ef0395827d36415eae
kbogdanski <kamil004@op.pl>
2016-10-02 14:13:06 +0200

    configure FOSUserBundle - add RegistrationType.php

---
+++ src/WebDiaryBundle/Form/RegistrationType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WebDiaryBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType {
+    
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
+    }
+
+    public function getParent() {
+        //return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix() {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName() {
+        return $this->getBlockPrefix();
+    }
+}
+
